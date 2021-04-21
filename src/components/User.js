338c6102360b7f01971c2e9dbb12b8f4aa36682e
Emon-Alam <66877658+Emon-Alam@users.noreply.github.com>
2021-04-21 11:05:25 +0600

    add and dlt

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+export default function User({name, id, dept, email, deleteUser}){
+	return (
+		<div className='usermain'>
+			<h3>Name: {name}</h3>
+			<p>
+				ID: {id} <br/>
+				DEPT: {dept} <br/>
+				Email: {email} <br/>
+			</p>			
+			<button onClick={()=>deleteUser(id)}>Delete</button>
+		</div>
+	)
+};
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import {useState} from 'react'; 
+import ReactDOM from 'react-dom';
+import './index.css';
+import {users} from './usersData';
+import User from './components/User';
+import AddUserFrom from './components/AddUserForm';
+
+function UserList(){
+
+	//const [name, setName] = useState('XYZ');
+	const [list, setUsers] = useState(users);
+
+	//add user 
+	const addUser= (newUser)=>{
+		setUsers([...list, newUser]);
+	}
+
+	//delete user
+	const deleteUser= (id)=>{
+		setUsers(list.filter((user)=>user.id !==id));
+	}
+
+	return (
+		<table>
+			<thead></thead>
+			<tbody>
+				<tr>
+					<td> <AddUserFrom addUser={addUser}/> </td>
+					<td>
+						{
+							list.map((user, index)=>{
+								return <User key={index} {...user} deleteUser={deleteUser} />;
+							})
+						}
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+}
+
+ReactDOM.render( <UserList/>, document.getElementById('root'))
